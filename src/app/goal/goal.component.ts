c6592a077e842565876ad7682d4511aedba99afc
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-13 01:35:25 +0300

    nest a child component with the parent

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -7,12 +7,32 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    { id: 1, name: 'watch finding Nemo' },
-    { id: 2, name: 'buy cookies' },
-    { id: 3, name: 'get new phone case' },
-    { id: 4, name: 'buy dog food' },
-    { id: 5, name: 'do math homework' },
-    { id: 6, name: 'plot my world domination plan' },
+    {
+      id: 1,
+      name: 'watch finding Nemo',
+      description: 'Find an online version and watch merlin find his son',
+    },
+    {
+      id: 2,
+      name: 'buy cookies',
+      description: 'I have to buy cookies for the parrot',
+    },
+    {
+      id: 3,
+      name: 'get new phone case',
+      description: 'Diana has her birthday coming up soon',
+    },
+    {
+      id: 4,
+      name: 'buy dog food',
+      description: 'Pupper likes expensive sancks',
+    },
+    { id: 5, name: 'do math homework', description: 'Damn math' },
+    {
+      id: 6,
+      name: 'plot my world domination plan',
+      description: "Cause I'm an evil overload",
+    },
   ];
   constructor() {}
 
