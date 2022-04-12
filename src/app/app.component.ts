2231cec78198de5bb983aad40e539a1bddd2cea5
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-13 00:46:51 +0300

    add class and import it to app component.ts; add an array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Goal } from './goal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,9 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  goals:string[]
+  // goals:string[]
+  goals:Goal[]=[
+    {id:1, name:'watch finding Nemo'},
+    {id:2, name:'buy cookies'},
+    {id:3,name:'get new phone case'},
+    {id:4,name:'buy dog food'},
+    {id:5, name:'do math homework'},
+    {id:6, name:'plot my world domination plan'}
+  ]
 
   constructor(){
-    this.goals=['watch an episode of finding Nemo', 'buy cookies', 'get new phone case']
+    // this.goals=['watch an episode of finding Nemo', 'buy cookies', 'get new phone case']
+
   }
 }
