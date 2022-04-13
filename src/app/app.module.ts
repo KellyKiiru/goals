c079a5d45463fd0825ca71bdff8a99c489f98235
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-04-13 04:20:51 +0300

    add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 import { StrikeThroughDirective } from './strike-through.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { StrikeThroughDirective } from './strike-through.directive';
     GoalComponent,
     GoalDetailComponent,
     StrikeThroughDirective,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -7,24 +7,46 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    new Goal (1, 'watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Goal (2, 'buy cookies', 'I have to buy cookies for the parrot'),
-    new Goal (3, 'get new phone case', 'Diana has her birthday coming up soon'),
-    new Goal (4, 'buy dog food', 'Pupper likes expensive sancks'),
-    new Goal (5,  'do math homework',  'Damn math' ),
-    new Goal (6, 'plot my world domination plan', "Cause I'm an evil overload"),
+    new Goal(
+      1,
+      'watch finding Nemo',
+      'Find an online version and watch merlin find his son',
+      new Date(2022, 3, 13)
+    ),
+    new Goal(
+      2,
+      'buy cookies',
+      'I have to buy cookies for the parrot',
+      new Date(2022, 6, 9)
+    ),
+    new Goal(
+      3,
+      'get new phone case',
+      'Diana has her birthday coming up soon',
+      new Date(2022, 5, 8)
+    ),
+    new Goal(
+      4,
+      'buy dog food',
+      'Pupper likes expensive sancks',
+      new Date(2023, 1, 14)
+    ),
+    new Goal(5, 'do math homework', 'Damn math', new Date(2023, 11, 11)),
+
+    // new Goal(6,'plot my world domination plan','Cause I am an evil overlord',new Date(1997,09,12)),
+
+
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
   constructor() {}
 
-  toggleDetails(index:number){
+  toggleDetails(index: number) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
-  completeGoal(isComplete: any, index: number){
-    if(isComplete){
-
-    this.goals.splice(index, 1)
+  completeGoal(isComplete: any, index: number) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
     }
-
   }
   ngOnInit(): void {}
 }
